7e65042f340c4ff364fd7cd0618f7ea3614e9348
CyrilArtFX <ccrozat@artfx.fr>
2023-08-30 13:39:24 +0200

    [training] rectangle with element buffer objects and indices

--- mcopengl/mcopengl/main.cpp
+++ mcopengl/mcopengl/main.cpp
@@ -103,23 +103,33 @@ int main()
 	glDeleteShader(fragmentShader);
 
 
-	//  triangle vertices data
+	//  triangles vertices data
 	float vertices[] =
 	{
-		-0.5f, -0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,
 		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+		-0.5f,  0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f
+	};
+	unsigned int indices[] =
+	{
+		0, 1, 2,
+		1, 3, 2
 	};
 
 	//  setup vertex buffer object and vertex array object
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO); //  bind the VAO before binding the vertex buffer, and before configuring vertex attributes
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -145,6 +155,7 @@ int main()
 		glUseProgram(shaderProgram); //  select shaders to use
 		glBindVertexArray(VAO); //  select VAO to use
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
 		//  events and buffer swap
@@ -156,6 +167,7 @@ int main()
 	//  delete all resources that are not necessary anymore (optionnal)
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 
