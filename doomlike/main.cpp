26e4fe416c2b0c77877a2d9a343032c1dac0e5bf
Cyril Crozat <ccrozat@artfx.fr>
2023-12-19 17:56:50 +0100

    [added] new project for doomlike

---
+++ doomlike/main.cpp
@@ -0,0 +1,17 @@
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <Core/engine.h>
+
+
+int main()
+{
+	/*Engine engine;
+	bool is_engine_init = engine.initialize();
+
+	if (!is_engine_init) return -1;
+
+	engine.run();
+	engine.close();*/
+
+	return 0;
+}
