ec836b5dedabc85d2bdb23f89ca546ac2a6dc07d
Cyril <ccrozat@artfx.fr>
2024-03-19 16:15:42 +0100

    [fixed] stairs collisions

--- doomlike/Decor/stairs.cpp
+++ doomlike/Decor/stairs.cpp
@@ -31,15 +31,13 @@ void StairsObj::setup(Vector3 position, Stairs::FacingDirection facingDirection)
 		//  no rotation
 
 		stairs_center = Vector3{ 1.03f, 1.11f, -0.93f };
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.0f, -0.9f}, Vector3{1.0f, 1.0f, 0.1f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.0f, 0.9f}, Vector3{1.0f, 1.0f, 0.1f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.625f, -0.875f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.375f, -0.625f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.125f, -0.375f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.125f, -0.125f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.375f, 0.125f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.625f, 0.375f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.875f, 0.625f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.125f, -0.875f, 0.0f}, Vector3{0.875f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.250f, -0.625f, 0.0f}, Vector3{0.750f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.375f, -0.375f, 0.0f}, Vector3{0.625f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.500f, -0.125f, 0.0f}, Vector3{0.500f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.625f, 0.125f, 0.0f}, Vector3{0.375f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.750f, 0.375f, 0.0f}, Vector3{0.250f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.875f, 0.625f, 0.0f}, Vector3{0.125f, 0.125f, 1.0f} }, this, false, "solid", false, false));
 
 		break;
 
@@ -47,15 +45,13 @@ void StairsObj::setup(Vector3 position, Stairs::FacingDirection facingDirection)
 		setRotation(Quaternion::fromEuler(Maths::toRadians(180.0f), 0.0f, 0.0f));
 
 		stairs_center = Vector3{ -1.03f, 1.11f, 0.93f };
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.0f, -0.9f}, Vector3{1.0f, 1.0f, 0.1f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.0f, 0.9f}, Vector3{1.0f, 1.0f, 0.1f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.625f, -0.875f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.375f, -0.625f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.125f, -0.375f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.125f, -0.125f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.375f, 0.125f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.625f, 0.375f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.875f, 0.625f, 0.0f}, Vector3{0.125f, 0.125f, 0.8f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.125f, -0.875f, 0.0f}, Vector3{0.875f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.250f, -0.625f, 0.0f}, Vector3{0.750f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.375f, -0.375f, 0.0f}, Vector3{0.625f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.500f, -0.125f, 0.0f}, Vector3{0.500f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.625f, 0.125f, 0.0f}, Vector3{0.375f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.750f, 0.375f, 0.0f}, Vector3{0.250f, 0.125f, 1.0f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.875f, 0.625f, 0.0f}, Vector3{0.125f, 0.125f, 1.0f} }, this, false, "solid", false, false));
 
 		break;
 
@@ -63,15 +59,13 @@ void StairsObj::setup(Vector3 position, Stairs::FacingDirection facingDirection)
 		setRotation(Quaternion::fromEuler(Maths::toRadians(270.0f), 0.0f, 0.0f));
 
 		stairs_center = Vector3{ 0.93f, 1.11f, 1.03f };
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.9f, 0.0f, 0.0f}, Vector3{0.1f, 1.0f, 1.0f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.9f, 0.0f, 0.0f}, Vector3{0.1f, 1.0f, 1.0f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.875f, 0.625f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.625f, 0.375f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.375f, 0.125f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.125f, -0.125f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.125f, -0.375f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.375f, -0.625f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.625f, -0.875f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.875f, -0.125f}, Vector3{1.0f, 0.125f, 0.875f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.625f, -0.250f}, Vector3{1.0f, 0.125f, 0.750f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.375f, -0.375f}, Vector3{1.0f, 0.125f, 0.625f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.125f, -0.500f}, Vector3{1.0f, 0.125f, 0.500f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.125f, -0.625f}, Vector3{1.0f, 0.125f, 0.375f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.375f, -0.750f}, Vector3{1.0f, 0.125f, 0.250f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.625f, -0.875f}, Vector3{1.0f, 0.125f, 0.125f} }, this, false, "solid", false, false));
 
 		break;
 
@@ -79,15 +73,13 @@ void StairsObj::setup(Vector3 position, Stairs::FacingDirection facingDirection)
 		setRotation(Quaternion::fromEuler(Maths::toRadians(90.0f), 0.0f, 0.0f));
 
 		stairs_center = Vector3{ -0.93f, 1.11f, -1.03f };
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{-0.9f, 0.0f, 0.0f}, Vector3{0.1f, 1.0f, 1.0f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.9f, 0.0f, 0.0f}, Vector3{0.1f, 1.0f, 1.0f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.875f, -0.625f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.625f, -0.375f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.375f, -0.125f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.125f, 0.125f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.125f, 0.375f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.375f, 0.625f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
-		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.625f, 0.875f}, Vector3{0.8f, 0.125f, 0.125f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.875f, 0.125f}, Vector3{1.0f, 0.125f, 0.875f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.625f, 0.250f}, Vector3{1.0f, 0.125f, 0.750f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.375f, 0.375f}, Vector3{1.0f, 0.125f, 0.625f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, -0.125f, 0.500f}, Vector3{1.0f, 0.125f, 0.500f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.125f, 0.625f}, Vector3{1.0f, 0.125f, 0.375f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.375f, 0.750f}, Vector3{1.0f, 0.125f, 0.250f} }, this, false, "solid", false, false));
+		Physics::CreateCollisionComponent(new BoxAABBColComp(Box{ stairs_center + Vector3{0.0f, 0.625f, 0.875f}, Vector3{1.0f, 0.125f, 0.125f} }, this, false, "solid", false, false));
 
 		break;
 	}
